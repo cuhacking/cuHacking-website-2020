4235357f32372f0fc2c9ca19648b2de25ffc3564
MrNickCasey <nicholas.casey@outlook.com>
2019-05-21 16:35:40 -0400

    Completed First Version
    Background, links, and basic responsive features all implemented.  Ready for deployment testing.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import logo from './logo.png'
-import hero from './trimmedLogo.png'
+import cuHackingLogo from './images/logo.png'
+import facebookLogo from './images/facebookLogo.png'
+import instagramLogo from './images/instagramLogo.png'
+import twitterLogo from './images/twitterLogo.png'
+import githubLogo from './images/githubLogo.png'
+import linkedinLogo from './images/linkedinLogo.png'
 import ParticleBackground from './ParticleBackground'
 
 
 class App extends React.Component {
     render() {
         return (
-          <div id = "content"> 
-            <ParticleBackground /> 
+          <div id = "content">
             <div id="leftSide">
+
                 <div id = "topNav">
-                    <img id="logo" src={logo} alt="Logo for cuHacking."></img>
+                    <img id="logo" src={cuHackingLogo} alt="Logo for cuHacking."></img>
                 </div>
+
                 <div id = "heading"> 
                     <h1> cuHacking is coming </h1>
                     <h1> January 2020 </h1>
                 </div>  
-                <div id = "bottomNav">
-                    <img id="MLHLogo" src="https://static.mlh.io/brand-assets/logo/official/mlh-logo-black.svg"></img>
-                </div>
-            </div> 
-            
-            <div id = "hero">
-                    <img src={hero}></img> 
-            </div>
 
-        </div>
+                <div className = "bottomNav">
+
+                    <div id="bottomNav-left"> 
+                        <a target="_blank" href="https://mlh.io/seasons/na-2020/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2020-season&utm_content=white"> <img id="MLHLogo" src="https://static.mlh.io/brand-assets/logo/official/mlh-logo-black.svg"></img> </a> 
+                    </div> 
+
+                    <div id="bottomNav-right">
+                        <a target="_blank" href="https://www.facebook.com/cuhacking/"> <img className ="socialLink" src={facebookLogo}></img> </a> 
+                        <a target="_blank" href="https://www.linkedin.com/company/cuhacking/"> <img className ="socialLink" src={linkedinLogo}></img> </a> 
+                        <a target="_blank" href="https://www.instagram.com/cuHacking/"> <img className ="socialLink" src={instagramLogo}></img> </a> 
+                        <a target="_blank" href="https://twitter.com/cuhacking?lang=en"> <img className ="socialLink" src={twitterLogo}></img> </a> 
+                        <a target="_blank" href="https://github.com/cuhacking"> <img className ="socialLink" src={githubLogo}></img> </a> 
+                    </div>
+
+                </div>
+            
+            </div> 
+
+            <div id="particles"> 
+                <ParticleBackground/>
+            </div> 
+
+        </div> 
 
         ) 
     }
