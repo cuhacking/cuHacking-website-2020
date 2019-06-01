18bc89ade6a66b67885deaba94ca11c39da982bc
Wal <wal.gatlei@gmail.com>
2019-05-31 21:12:49 -0400

    Add newlines to file ends

--- src/components/cuHackingLogo.js
+++ src/components/cuHackingLogo.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import cuHackingLogo from 'images/logo.svg'
-
-const style = { 
-  width: "45px", 
-  height: "45px"
-}
+import styles from './cuHackingLogo.module.css';
 
 export default () => (
-  <img style={style} id="logo" src={cuHackingLogo} alt="cuHacking"></img>
+  <img id={styles.logo} src={cuHackingLogo} alt="cuHacking"></img>
 );
