ee1918705a2474b9b61a242531a7acaa6c24ca3f
Nick Casey <nicholas.casey@outlook.com>
2019-10-07 00:37:50 -0400

    Revert "Revert "d""
    This reverts commit d147f33acd0f06162fa43ec2321aa4b44b9865df.

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import CuHeader from './cuHeader.js';
-import { createRequireFromPath } from 'module';
+import logoFullscript from '../assets/images/sponsors/fullscript.svg';
+import logoLightHouse from '../assets/images/sponsors/lhl-logo.png'; 
+import logoCiena from '../assets/images/sponsors/ciena.svg';
+import logoCSE from '../assets/images/sponsors/cse.png';
+
 
 class Sponsor extends Component {
   constructor(props) {
@@ -38,13 +42,14 @@ export default class Sponsors extends Component {
         </div>
         <div className="giga"></div>
         <div className="mega">
-          <Sponsor alt="Fullscript Logo" src='../assets/images/sponsors/fullscript.svg' url="https://fullscript.com/"/>
-          <Sponsor alt="Lighthouse Labs" src='' url="https://www.lighthouselabs.ca/"/>
-          <Sponsor alt="Ciena" src='' url="https://fullscript.com/"/>
-          <Sponsor alt="CSE" src='' url="https://www.lighthouselabs.ca/"/>
+          <Sponsor alt="Fullscript Logo" src={logoFullscript} url="https://fullscript.com/"/>          
+          <Sponsor alt="Lighthouse Labs" src={logoLightHouse} url="https://www.lighthouselabs.ca/"/>
+          <Sponsor alt="Ciena" src={logoCiena} url="https://www.ciena.com/"/>
+          <Sponsor alt="CSE" src={logoCSE} url="https://www.cse-cst.gc.ca/en"/>
         </div>
         <div className="kilo">
-          <Sponsor alt="CSE" src='' url="https://www.lighthouselabs.ca/"/>
+          <Sponsor alt="FDM Group" src='' url="#"/>
+
         </div>
         <p className="bodyText"> Interested in sponsoring? Contact us at <a href="mailto:sponsorship@cuhacking.com"> sponsorship@cuhacking.com  </a> </p> 
     </div> 
