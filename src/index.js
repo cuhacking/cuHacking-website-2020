77d3917beae441d2c6d838aec3b40947b9df2dde
Nick Casey <nicholas.casey@outlook.com>
2019-11-17 21:44:03 -0500

    Added React Router
    Navigate to '/' for home page, /login for login page, and '/dashboard' for the dashboard.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Home from 'pages/home';
-// import Dashboard from 'pages/dashboard';
-import Login from 'pages/login'; 
+import Home from 'pages/home';
+import Login from 'pages/login';
+import Dashboard from 'pages/dashboard';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
 import './index.css';
 
 const App = () => (
-  <Login/>
+  <Router>   
+    <Switch> 
+      <Route path="/" exact component={Home}/>
+      <Route path="/login" component={Login}/> 
+      <Route path="/dashboard" component={Dashboard}/> 
+    </Switch>
+  </Router>
 );
 
 ReactDOM.render(<App/>, document.getElementById('root'));
