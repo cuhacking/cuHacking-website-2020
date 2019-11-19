afb95de23cffd7dfcc23eaf0b844836c40c926f5
Nick Casey <nicholas.casey@outlook.com>
2019-11-19 12:31:47 -0500

    Rough-In of Dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,8 +1,69 @@
 import React from 'react';
-import 'index.css';
+import styles from './index.module.css';
+import {
+  Input, 
+  Button, 
+} from 'components';
+
+function SideNav() {
+  return (
+    <div className={styles.sideNav}>
+      <ul>
+        <li> logo </li>
+        <li> start </li>
+        <li> stuff </li> 
+        <li> logo </li>3
+      </ul>
+    </div>
+  )
+}
+
+function Start() {
+
+  function handleNext(event) {
+    event.preventDefault(); 
+    console.log('switch to about you in a graceful way'); 
+  }
+
+  return (
+    <div className={styles.applicationPage} id="start">
+      <h1>cuHacking 2020</h1> 
+      <h2>Application</h2> 
+      <form className={styles.nextButton} onSubmit={handleNext}>
+        <Button label="Start"/>
+      </form>
+    </div>
+  )
+}
+
+function basicInfo() {
+
+
+}
+
+function aboutYou() {
+
+
+}
+
+function skillsFeats() {
+
+
+}
+
+function profile() {
+
+
+}
+
+function submit() {
+
+
+}
 
 export default () => (
-  <>
-      <h2>application form </h2>
-  </>
+  <div className={styles.dashboard}>
+    <SideNav />
+    <Start />
+  </div>
 );
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -7,35 +7,43 @@ import {
 } from 'components';
 import styles from './index.module.css';
 
-function Login() {
+class Login extends React.Component {
   // Initial login page to the dashboard, ask for email/password by default. 
   // To think about - do we want to add Sign in with Google/Apple/etc...
   // This should also branch to a create account page if they don't already have one? 
 
-  function handleSubmit(event) {
+  constructor(props) {
+    super(props);
+    this.state = {  email: '',          
+                    password: 'before',          
+                }; 
+    this.handleSubmit = this.handleSubmit.bind(this);
+}
+
+  handleSubmit(event) {
     event.preventDefault(); 
-    console.log(event);      
+    this.props.history.push("dashboard");
     /* this is where we handle the all the logic for attempting (and maybe succeeding) to sign in */ 
+    // for now, we're going to say if the email is 'email@email.com' and the password is p then 
+    // succeed - have to figure this out on the APi after. 
   }
 
-  return (
-    <div className={styles.loginPage}>
-      <Navbar /> 
-      <div className={styles.loginContainer}> 
-        <h2>Welcome to cuHacking!</h2> 
-        <form onSubmit={handleSubmit} > 
-          <Input type="email" label="Email"/>
-          <Input type="password" label="Password"/>
-          <Button type="submit" label="Login"/> 
-        </form>
+  render () {
+    return (
+      <div className={styles.loginPage}>
+        <Navbar /> 
+        <div className={styles.loginContainer}> 
+          <h2>Welcome to cuHacking!</h2> 
+          <form onSubmit={this.handleSubmit} > 
+            <Input type="email" label="Email" />
+            <Input type="password" label="Password" />
+            <Button type="submit" label="Login"/> 
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 
 }
 
-export default () => (
-  <>
-      <Login /> 
-  </>
-);
+export default Login; 
