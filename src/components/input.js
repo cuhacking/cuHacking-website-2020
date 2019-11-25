2cfb2251840ec8215c5936417e0bbbc4da6e7370
walsker <walskerw@gmail.com>
2019-11-24 21:06:34 -0500

    Create input component for long answers

--- src/components/input.js
+++ src/components/input.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import styles from './input.module.css';
 
-const Input = ({label, type, value, onChange}) => (
-  <div className={styles.inputContainer}> 
-    <input className={styles.input} type={type} label={label} value={value} onChange={onChange}/>
-    <p className={styles.label}>{label}</p>
-  </div> 
-);
+const Input = ({label, inputStyle, ...props}) => {
+  switch (inputStyle) {
+    case 'long':
+      return (
+        <div className={styles.inputContainer}>
+          <textarea className={styles.longInput} rows={4} label={label} {...props} />
+          <p className={styles.label}>{label}</p>
+        </div>
+      )
+    default:
+      return (
+        <div className={styles.inputContainer}>
+          <input className={styles.input} label={label} {...props}/>
+          <p className={styles.label}>{label}</p>
+        </div>
+      )
+  }
+};
 
-export default Input; 
+export default Input;
