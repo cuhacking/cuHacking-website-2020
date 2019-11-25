0f26331e98e9458ebccd5ce32c128705429aa95c
walsker <walskerw@gmail.com>
2019-11-25 01:13:25 -0500

    Create drop box component for resumes

---
+++ src/pages/dashboard/dropbox.js
@@ -0,0 +1,35 @@
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCloudUploadAlt, faFileAlt} from '@fortawesome/free-solid-svg-icons';
+import styles from './dropbox.module.css'
+
+const Dropbox = ({resume, setResume=()=>console.error('setResume not provided')}) => {
+  const onDrop = useCallback(acceptedFiles => {
+    setResume(acceptedFiles[0])
+  }, [setResume])
+
+  const {getRootProps, getInputProps} = useDropzone({
+    onDrop,
+    accept: 'application/pdf'
+  })
+
+  if (!resume) {
+    return (
+      <div {...getRootProps({className: styles.emptyBox})}>
+        <input {...getInputProps()}/>
+        <FontAwesomeIcon icon={faCloudUploadAlt} size='5x'/>
+        <p>Drop your resume here, or click to select it. (PDF only)</p>
+      </div>
+    )
+  } else {
+    return (
+      <div className={styles.fullBox} onClick={() => setResume(undefined)}>
+        <FontAwesomeIcon icon={faFileAlt} size='5x'/>
+        <p className={styles.file}><i>{resume.path}</i></p>
+      </div>
+    )
+  }
+}
+
+export default Dropbox
