cce8cbdc6a8ddc87b4a856b1f0752adc14711b93
walsker <walskerw@gmail.com>
2019-11-24 19:24:28 -0500

    Create basic input and button components

--- src/components/input.js
+++ src/components/input.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import styles from './input.module.css';
 
-class Input extends React.Component {
-    render() {
-        return (
-            <div className={styles.inputContainer}> 
-                <input className={styles.input} type={this.props.type} label={this.props.label}>
-                </input>
-                <p className={styles.label}>{this.props.label}</p>
-            </div> 
-        );  
-    }
-}
+const Input = ({label, type, value, onChange}) => (
+  <div className={styles.inputContainer}> 
+    <input className={styles.input} type={type} label={label} value={value} onChange={onChange}/>
+    <p className={styles.label}>{label}</p>
+  </div> 
+);
 
 export default Input; 
