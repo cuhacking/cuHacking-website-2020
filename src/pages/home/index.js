0e2ee379a0c94c5a94d0f3fb33cd460def6694d8
Nick Casey <nicholas.casey@outlook.com>
2019-11-26 00:06:53 -0500

    Removed Temporary Links

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -106,17 +106,7 @@ export default () => (
   <>
     <Navbar/>
     <div id={styles.content}>
-      <Landing/>
-      
-      <ul>
-        <Link to="/login">
-          <li> login </li>
-        </Link>
-        <Link to="/dashboard">
-          <li> dashboard </li>
-        </Link>
-      </ul>
-      
+      <Landing/>      
       <Sponsors/>
     </div>
     <Footer/> 
