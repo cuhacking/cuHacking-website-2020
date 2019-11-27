739edcdd26ce3f25923121337026650b78c3ff95
Nick Casey <nicholas.casey@outlook.com>
2019-11-26 23:28:51 -0500

    Added Password Strength in Create Form

--- src/pages/create/index.js
+++ src/pages/create/index.js
@@ -6,6 +6,7 @@ import {
     Navbar
 } from 'components';
 import styles from './create.module.css';
+import zxcvbn from 'zxcvbn'; 
 
 class Create extends React.Component {
   // Initial login page to the application, ask for email/password by default. 
@@ -22,6 +23,39 @@ class Create extends React.Component {
                 }; 
     this.handleChange = this.handleChange.bind(this); 
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  PasswordStrengthMeter() {
+
+    var strength;
+    (this.state.password.length >= 8) ? strength = zxcvbn(this.state.password).score
+                                      : strength = -1; 
+
+    function label(strength) {
+      switch(strength) {
+        case -1: 
+          return 'Too short'; 
+        case 0:
+          return 'Weak';
+        case 1:
+          return 'Weak';
+        case 2:
+          return 'Fair';
+        case 3:
+          return 'Good';
+        case 4:
+          return 'Strong'; 
+        default: 
+          return 'Weak'; 
+      }
+    }
+
+    return (
+      <div className={styles.PasswordStrengthMeter}>
+        <br/> 
+        <strong>Password strength: </strong> {label(strength)}
+      </div>    
+    )
   }
 
   validatePassword() {
@@ -93,9 +127,10 @@ class Create extends React.Component {
           <h2>Create your cuHacking account.</h2> 
           <p>Already have an account? <a href="/login"> Click here to login.</a></p>
           <form className={styles.loginContainer} onSubmit={this.handleSubmit}> 
-            <Input name="email"     type="email"    label="Email"                                     value={this.state.email}      onChange={this.handleChange} required={true}/>
-            <Input name="pw"        type="password" label="Create a password. (Minimum 8 characters)" value={this.state.password}   onChange={this.handleChange} required={true}/>
-            <Input name="pw2"       type="password" label="Verify your password."                     value={this.state.password2}  onChange={this.handleChange} required={true}/>     
+            <Input placeholder="email@example.com" name="email"     type="email"    label="Email"                                     value={this.state.email}      onChange={this.handleChange} required={true}/>
+            <Input placeholder="Password" name="pw"        type={this.state.hidden ? "password" : "text"} label="Create a password. (Minimum 8 characters)" value={this.state.password}   onChange={this.handleChange} required={true}/>
+            <Input placeholder="Confirm"  name="pw2"       type="password" label="Confirm your password."                     value={this.state.password2}  onChange={this.handleChange} required={true}/>     
+            {this.PasswordStrengthMeter()}
             <Button type="submit" label="Create Account" disabled={!this.state.validForm}/>      
             <p id="error-message"> {this.state.error} </p>
           </form>
