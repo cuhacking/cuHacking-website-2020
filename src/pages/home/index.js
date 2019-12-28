a1b75d2afbe6a772b88d1553a6b03ff37fc35b5d
walsker <walskerw@gmail.com>
2019-12-28 13:26:46 -0500

    Start third wave of applications

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -47,9 +47,9 @@ const Landing = () => (
         <p className={styles.eventInfo}>Richcraft Hall · Carleton University</p>
       </div>
       <div className={styles.landingButtons}>
-        <Button external link to='https://my.cuhacking.com' disabled label='Apply Now' />
+        <Button external link to='https://my.cuhacking.com' label='Apply Now' />
       </div>
-      <p className={styles.notice}>Applications are now closed.</p>
+      <p className={styles.notice}>Applications close January 5 at 11:59pm EST.</p>
     </div>
   </div>
 )
